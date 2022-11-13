776b5c8c1edd82b9dde6a9607c43d0a8305c1b53
max-loktionov <studreno@gmail.com>
2022-11-13 02:18:22 +0200

    fix: del others

--- controllers/friends/getAllFriends.js
+++ controllers/friends/getAllFriends.js
@@ -1,7 +1,7 @@
 const { Friends } = require("../../models/friends");
 
 const getAllFriends = async (req, res) => {
-  const { page = 1, limit = 10, ...query } = req.query;
+  const { page = 1, limit = 10 } = req.query;
   const skip = (page - 1) * limit;
 
   const friends = await Friends.find({}, "", { skip, limit });
