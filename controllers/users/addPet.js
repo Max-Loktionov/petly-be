9a45bf452d35d6315fa7c60529403ff6a49b39eb
Sarviro <annka_nuha@i.ua>
2022-11-19 04:01:25 +0200

    fix bug/delete userpets

--- controllers/users/addPet.js
+++ controllers/users/addPet.js
@@ -2,25 +2,29 @@ const { Pet } = require("../../models/pets");
 const Jimp = require("jimp");
 const fs = require("fs/promises");
 const path = require("path");
-
+const { ObjectId } = require("mongodb");
 const avatarDir = path.join(__dirname, "../../", "public", "pets");
 const imgSizePx = 250;
 
 const addPet = async (req, res) => {
   const { _id } = req.user;
-  const { filepath: tempUpload, originalFilename } = req.files.avatar;
-  const jimpAvatar = await Jimp.read(tempUpload);
-  await jimpAvatar.resize(imgSizePx, imgSizePx, Jimp.RESIZE_BEZIER).writeAsync(tempUpload);
+  const newId = new ObjectId(_id);
+  const avatar = null;
+  if (req.files.avatar) {
+    const { filepath: tempUpload, originalFilename } = req.files.avatar;
+    const jimpAvatar = await Jimp.read(tempUpload);
+    await jimpAvatar.resize(imgSizePx, imgSizePx, Jimp.RESIZE_BEZIER).writeAsync(tempUpload);
 
-  const extention = originalFilename.split(".").pop();
+    const extention = originalFilename.split(".").pop();
 
-  const filename = `${_id}.${extention}`;
-  const resultUpload = path.join(avatarDir, filename);
+    const filename = `${_id}.${extention}`;
+    const resultUpload = path.join(avatarDir, filename);
 
-  await fs.rename(tempUpload, resultUpload);
-  const avatar = path.join("pets", filename);
+    await fs.rename(tempUpload, resultUpload);
+    avatar = path.join("pets", filename);
+  }
 
-  const newPat = await Pet.create({ ...req.body, avatar, owner: _id });
+  const newPat = await Pet.create({ ...req.body, avatar, owner: newId });
 
   res.status(201).json({
     message: "success",
